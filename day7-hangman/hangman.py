9a350e9cd95782b559a1c00daf2f2ee1b27ade0b
Alex Gose <alexgose@users.noreply.github.com>
2023-03-14 11:47:57 -0400

    Add the ability for the user to keep guessing
    Message "You win." is printed when all the letters are guessed

--- day7-hangman/hangman.py
+++ day7-hangman/hangman.py
@@ -13,19 +13,13 @@ print(f'Pssst, the solution is {chosen_word}.')
 # ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 display = ['_' for _ in range(len(chosen_word))]
 
-guess = input("Guess a letter: ").lower()
+# Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 
-# Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in
-#the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then
-# display should be ["_", "p", "p", "_", "_"].
-for index, letter in enumerate(chosen_word):
-    if letter == guess:
-        display[index] = letter
+while '_' in display:
+    guess = input("Guess a letter: ").lower()
+    for index, letter in enumerate(chosen_word):
+        if letter == guess:
+            display[index] = letter
+    print(display)
 
-# Print 'display' and you should see the guessed letter in the correct
-# position and every other letter replace with "_".
-# Hint - Don't worry about getting the user to guess the next letter.
-# We'll tackle that in step 3.
-print(display)
+print("You win.")
