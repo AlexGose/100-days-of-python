d96a5619a74af3d31adbb0f1aa8bd00c9dd49653
Alex Gose <alexgose@users.noreply.github.com>
2023-03-15 11:59:24 -0400

    Add ACII art logo

--- day8-caesar-cipher/caesar_cipher.py
+++ day8-caesar-cipher/caesar_cipher.py
@@ -1,7 +1,10 @@
 #!/bin/env python3
 
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print(art.logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
