fff058d25342eb6affcf797f0703234b918abb89
Alex Gose <alexgose@users.noreply.github.com>
2023-03-15 11:25:32 -0400

    Add ability to encode

--- day8-caesar-cipher/caesar_cipher.py
+++ day8-caesar-cipher/caesar_cipher.py
@@ -5,3 +5,18 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+def encrypt(text, shift):
+    output = ''
+    for letter in text:
+        if letter != ' ':
+            index = alphabet.index(letter)
+            index += shift
+            index %= 26
+            output += alphabet[index]
+        else:
+            output += ' '
+    print(f"The encoded text is {output}")
+
+if direction.lower() == 'encode':
+    encrypt(text, shift)
