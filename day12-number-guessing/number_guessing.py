69f8dbb1df0d2eabee620be00872637f30c51065
Alex Gose <alexgose@users.noreply.github.com>
2023-03-24 09:07:04 -0400

    Add introduction, test code, and user input

--- day12-number-guessing/number_guessing.py
+++ day12-number-guessing/number_guessing.py
@@ -1,6 +1,20 @@
 #!/bin/env python3
 
 from art import logo
+from random import randint
 
 if __name__ == '__main__':
     print(logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    number = randint(1,100)
+
+    print(f"Pssst, the correct answer is {number}") # temp test code
+
+    difficulty = input("Choose a difficulty.  Type 'easy' or 'hard': ")
+
+    if difficulty.lower() == 'easy':
+        num_attempts = 10
+    else:
+        num_attempts = 5
