01cd8b00e3308a5406770233e2cdd496739681e2
Alex Gose <alexgose@users.noreply.github.com>
2023-03-24 09:32:42 -0400

    Add game logic and remove temp test code

--- day12-number-guessing/number_guessing.py
+++ day12-number-guessing/number_guessing.py
@@ -10,11 +10,34 @@ if __name__ == '__main__':
 
     number = randint(1,100)
 
-    print(f"Pssst, the correct answer is {number}") # temp test code
-
     difficulty = input("Choose a difficulty.  Type 'easy' or 'hard': ")
 
     if difficulty.lower() == 'easy':
         num_attempts = 10
     else:
         num_attempts = 5
+
+    done = False
+    while not done:
+        print(f"You have {num_attempts} attempt", end='')
+        if num_attempts > 1:
+            print("s", end='')
+        print(" remaining to guess the number.")
+            
+        guess = int(input("Make a guess: "))
+        num_attempts -= 1
+
+        if guess < number:
+            print("Too low.")
+        elif guess > number:
+            print("Too high.")
+        else:
+            print(f"You got it! The answer was {number}.")
+            done = True
+
+        if guess != number:
+            if num_attempts == 0:
+                print("You've run out of guesses, you lose.")
+                done = True
+            else:
+                print("Guess again.")
