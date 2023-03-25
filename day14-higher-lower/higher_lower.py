b369f9461f67e5041ff0ca8a9b0f87fb35c7d93f
Alex Gose <alexgose@users.noreply.github.com>
2023-03-25 11:34:13 -0400

    Outline steps using comments

--- day14-higher-lower/higher_lower.py
+++ day14-higher-lower/higher_lower.py
@@ -4,3 +4,15 @@ from art import logo
 
 if __name__ == '__main__':
     print(logo)
+
+    # Main loop 
+
+    # Randomly select two instagram pages and the number of followers for each
+
+    # Print the names of two instagram pages
+
+    # Ask the user which instagram page has more followers
+
+    # If answer is correct, tell user and print current score
+
+    # If answer is wrong, tell user, print final score, and finish loop
