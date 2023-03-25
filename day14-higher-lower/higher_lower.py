297ecf9df1c6178f9f351559705de81438d30e92
Alex Gose <alexgose@users.noreply.github.com>
2023-03-25 11:52:45 -0400

    Randomly select data for two instagram pages

--- day14-higher-lower/higher_lower.py
+++ day14-higher-lower/higher_lower.py
@@ -1,18 +1,42 @@
 #!/bin/env python3
 
 from art import logo
+from game_data import data
+from random import randint
+
+
+def rand_indices(N):
+    """Return two unordered random indices between 0 and N-1"""
+    first_index = randint(0, N-1)
+    second_index = randint(0, N)
+    if second_index >= first_index:
+        second_index += 1
+    return first_index, second_index
+
+
+def get_insta_pages(data):
+    """Return data for two unordered random instagram pages"""
+    A_index, B_index = rand_indices(len(data))
+    return data[A_index], data[B_index]
+
 
 if __name__ == '__main__':
     print(logo)
 
-    # Main loop 
+    done = False
+    while not done:
+        # Randomly select two instagram pages
+        A_data, B_data = get_insta_pages(data)
 
-    # Randomly select two instagram pages and the number of followers for each
+        # temp test code
+        print(A_data)
+        print(B_data)
+        break
 
-    # Print the names of two instagram pages
+        # Print the names of two instagram pages
 
-    # Ask the user which instagram page has more followers
+        # Ask the user which instagram page has more followers
 
-    # If answer is correct, tell user and print current score
+        # If answer is correct, tell user and print current score
 
-    # If answer is wrong, tell user, print final score, and finish loop
+        # If answer is wrong, tell user, print final score, and finish loop
