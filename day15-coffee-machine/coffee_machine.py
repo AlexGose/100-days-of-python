c770d5a83d9fa088e6c5f569c6cea721e3e97210
Alex Gose <alexgose@users.noreply.github.com>
2023-03-27 09:49:50 -0400

    Add main script with menu, resources, and TODOs

---
+++ day15-coffee-machine/coffee_machine.py
@@ -0,0 +1,56 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+if __name__ == '__main__':
+    # temp test code
+    print(MENU)
+    print(resources)
+
+    # TODO: 1. prompt user for beverage choice
+
+    # TODO: 2. 'off' turns off the machine
+
+    # TODO: 3. 'report' prints resources
+
+    # TODO: 4. print message if insufficient resources after beverage choice
+
+    # TODO: 5. if sufficient resources after choice, prompt for coins
+
+    # TODO: 6. process coins
+
+    # TODO: 7. print message if insufficient money and give refund
+
+    # TODO: 8. if sufficient money, give any change and add to revenue
+
+    # TODO: 9. Make coffee, deduct ingredients from inventory
+
+    # TODO: 10. Give beverage to customer
