7088db5bde1880f0241a2978b87a852191c52d23
Alex Gose <alexgose@users.noreply.github.com>
2023-03-28 12:24:53 -0400

    Add user prompt for drink or 'off' to stop

--- day16-oop/main.py
+++ day16-oop/main.py
@@ -1,3 +1,15 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+cm = CoffeeMaker()
+mm = MoneyMachine()
+m = Menu()
+
+done = False
+
+while not done:
+    drink = input(f"What would you like? ({m.get_items()}): ").lower()
+
+    if drink == 'off':
+        done = True
