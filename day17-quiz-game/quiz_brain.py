38826ef2fb9d3127676c2c2e61fb225afa6fa26d
Alex Gose <alexgose@users.noreply.github.com>
2023-03-29 11:06:59 -0400

    Print next question and take user input

--- day17-quiz-game/quiz_brain.py
+++ day17-quiz-game/quiz_brain.py
@@ -3,5 +3,8 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = question_list
 
-
-print(QuizBrain(['a', 'b']))  # temp test code
+    def next_question(self):
+        user_answer = input(f"Q.{self.question_number + 1}: {self.question_list[self.question_number].text} "
+                            + f"(True/False)?: ")
+        self.question_number += 1
+        return user_answer
