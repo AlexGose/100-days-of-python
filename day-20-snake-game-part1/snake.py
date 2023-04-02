d5c77080b85506fecef23f39a08b83dab83b240c
Alex Gose <alexgose@users.noreply.github.com>
2023-04-02 15:31:25 -0400

    Add user control of snake with arrow keys

--- day-20-snake-game-part1/snake.py
+++ day-20-snake-game-part1/snake.py
@@ -1,4 +1,9 @@
 from turtle import Turtle
+MOVE_DISTANCE = 20
+UP_ANGLE = 90
+DOWN_ANGLE = 270
+LEFT_ANGLE = 180
+RIGHT_ANGLE = 0
 
 
 class Snake:
@@ -10,11 +15,27 @@ class Snake:
             next_turtle.penup()
             next_turtle.setx(next_turtle.xcor() - 20 * turtle_index)
             self.segments.append(next_turtle)
+        self.head = self.segments[0]
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].left(90)
-        self.segments[0].forward(20)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN_ANGLE:
+            self.head.setheading(UP_ANGLE)
+
+    def down(self):
+        if self.head.heading() != UP_ANGLE:
+            self.head.setheading(DOWN_ANGLE)
+
+    def left(self):
+        if self.head.heading() != RIGHT_ANGLE:
+            self.head.setheading(LEFT_ANGLE)
+
+    def right(self):
+        if self.head.heading() != LEFT_ANGLE:
+            self.head.setheading(RIGHT_ANGLE)
