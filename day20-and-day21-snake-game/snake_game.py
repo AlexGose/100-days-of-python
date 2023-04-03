337ba533fcd3532f21b871d32461183d428d6dc7
Alex Gose <alexgose@users.noreply.github.com>
2023-04-03 10:31:05 -0400

    Refresh food after snake collides with it

--- day20-and-day21-snake-game/snake_game.py
+++ day20-and-day21-snake-game/snake_game.py
@@ -24,7 +24,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
