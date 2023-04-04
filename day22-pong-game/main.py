4000044fe30c425ab1212ad7cc36010cb214ece8
Alex Gose <alexgose@users.noreply.github.com>
2023-04-04 17:58:26 -0400

    Add moving ball to the Pong court

--- day22-pong-game/main.py
+++ day22-pong-game/main.py
@@ -2,14 +2,19 @@
 
 from court import Court
 from paddle import Paddle
+from ball import Ball
+import time
 
 court = Court()
 r_paddle = Paddle(court, "Up", "Down", 350, 0)
 l_paddle = Paddle(court, "w", "s", -350, 0)
+ball = Ball()
 
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
+    ball.move()
     court.update()
 
 court.exit_on_click()
