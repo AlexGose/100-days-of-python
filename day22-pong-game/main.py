1b7e9dc09315bb5380fb7e523e2171cab24bacd4
Alex Gose <alexgose@users.noreply.github.com>
2023-04-05 15:36:50 -0400

    Add scoreboard

--- day22-pong-game/main.py
+++ day22-pong-game/main.py
@@ -3,20 +3,24 @@
 from court import Court
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 court = Court()
 r_paddle = Paddle(court, "Up", "Down", 350, 0)
 l_paddle = Paddle(court, "w", "s", -350, 0)
 ball = Ball(court)
+scoreboard = Scoreboard()
 
 game_is_on = True
 
 while game_is_on:
     time.sleep(0.1)
     if ball.is_off_court_left():
+        scoreboard.r_point()
         ball.reset_right()
     if ball.is_off_court_right():
+        scoreboard.l_point()
         ball.reset_left()
     ball.bounce_left_if_hitting(r_paddle)
     ball.bounce_right_if_hitting(l_paddle)
