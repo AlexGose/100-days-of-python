a0e7b29acd3e8411c2c8de673cdaa613f1664434
Alex Gose <alexgose@users.noreply.github.com>
2023-04-08 12:59:25 -0400

    Save and retrieve high score from file

--- day24-snake-and-mail-merge/scoreboard.py
+++ day24-snake-and-mail-merge/scoreboard.py
@@ -9,13 +9,24 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = Scoreboard.read_high_score()
         self.pencolor("white")
         self.hideturtle()
         self.penup()
         self.goto(0, 270)
         self.pendown()
         self.show_score()
+
+    @staticmethod
+    def read_high_score():
+        with open('data.txt') as file:
+            high_score = int(file.read())
+        return high_score
+
+    @staticmethod
+    def write_high_score(high_score):
+        with open('data.txt', 'w') as file:
+            file.write(str(high_score))
 
     def show_score(self):
         self.clear()
@@ -24,6 +35,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            Scoreboard.write_high_score(self.high_score)
         self.score = 0
         self.show_score()
 
