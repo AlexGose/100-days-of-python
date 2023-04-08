a4326824bb7bdfe1050fbd68cc0fd9c91e477282
Alex Gose <alexgose@users.noreply.github.com>
2023-04-08 13:48:28 -0400

    Add code for mail merge project

--- day24-snake-and-mail-merge/main.py
+++ day24-snake-and-mail-merge/main.py
@@ -1,8 +1,17 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+def get_names():
+    with open('Input/Names/invited_names.txt') as names:
+        lines = names.readlines()
+    return [line.strip() for line in lines]
+
+
+def create_letter(name):
+    with open('Input/Letters/starting_letter.txt') as template:
+        lines = template.readlines()
+    lines[0] = lines[0].replace('[name]', name)
+    with open(f'Output/ReadyToSend/letter_for_{name}.txt', 'w') as letter:
+        letter.writelines(lines)
+
+
+for invitee in get_names():
+    create_letter(invitee)
