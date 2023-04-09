7ab52835a14a9e3c82649941bffb73a395c26df5
Alex Gose <alexgose@users.noreply.github.com>
2023-04-09 11:27:31 -0400

    Write correct state names onto map

--- day25-states-game/main.py
+++ day25-states-game/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,7 +7,25 @@ image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+writer = turtle.Turtle()
+writer.penup()
+writer.hideturtle()
+
+data = pandas.read_csv('50_states.csv')
+
+
+def write_state_name(state_name, states_data, state_writer):
+    data_row = states_data[states_data['state'] == state_name]
+    state_writer.goto(float(data_row['x']), float(data_row['y']))
+    state_writer.write(state_name)
+
+
+num_correct = 0
+while num_correct < 50:
+    answer_state = screen.textinput(title=f"{num_correct}/50 States Correct", prompt="What's another state's name?")
+    answer_state = answer_state.title()
+    if data['state'].isin([answer_state]).any():
+        num_correct += 1
+        write_state_name(answer_state, data, writer)
 
 screen.exitonclick()
