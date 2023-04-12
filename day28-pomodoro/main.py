09300e1578315239854194a9fb437061d621da79
Alex Gose <alexgose@users.noreply.github.com>
2023-04-12 11:49:59 -0400

    Add work and break intervals to timer

--- day28-pomodoro/main.py
+++ day28-pomodoro/main.py
@@ -8,13 +8,30 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5)
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0 or reps % 8 == 2 or reps % 8 == 4 or reps % 8 == 6:
+        reps += 1
+        timer_label.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+    elif reps % 8 == 7:
+        reps += 1
+        timer_label.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    else:
+        reps += 1
+        timer_label.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
@@ -23,7 +40,9 @@ def count_down(count):
     count_sec = count % 60
     canvas.itemconfig(time_text, text=f"{count_min}:{count_sec:02d}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        window.after(10, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
