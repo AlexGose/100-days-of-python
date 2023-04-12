53b186530fe21feecd16c6181234c850c91eef06
Alex Gose <alexgose@users.noreply.github.com>
2023-04-12 11:33:56 -0400

    Add countdown and timer

--- day28-pomodoro/main.py
+++ day28-pomodoro/main.py
@@ -11,9 +11,20 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
+
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = count // 60
+    count_sec = count % 60
+    canvas.itemconfig(time_text, text=f"{count_min}:{count_sec:02d}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
@@ -23,13 +34,13 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+time_text = canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.grid(row=1, column=1)
 
 timer_label = tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25, "bold"))
 timer_label.grid(row=0, column=1)
 
-start_button = tkinter.Button(text="Start")
+start_button = tkinter.Button(text="Start", command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = tkinter.Button(text="Reset")
