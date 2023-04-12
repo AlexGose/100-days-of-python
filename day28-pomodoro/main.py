e72cf8bb7aa5172b65698287323a7c64fec5b240
Alex Gose <alexgose@users.noreply.github.com>
2023-04-12 13:17:14 -0400

    Add reset functionality
    Also slow timer back to normal speed

--- day28-pomodoro/main.py
+++ day28-pomodoro/main.py
@@ -9,8 +9,18 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
+
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    global timer
+    global reps
+    window.after_cancel(timer)
+    canvas.itemconfig(time_text, text="00:00")
+    timer_label.config(text="Timer", fg=GREEN)
+    checks_label.config(text="")
+    reps = 0
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
@@ -37,11 +47,12 @@ def start_timer():
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     global reps
+    global timer
     count_min = count // 60
     count_sec = count % 60
     canvas.itemconfig(time_text, text=f"{count_min}:{count_sec:02d}")
     if count > 0:
-        window.after(10, count_down, count - 1)
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         if reps % 2 == 0:
@@ -65,7 +76,7 @@ timer_label.grid(row=0, column=1)
 start_button = tkinter.Button(text="Start", command=start_timer)
 start_button.grid(row=2, column=0)
 
-reset_button = tkinter.Button(text="Reset")
+reset_button = tkinter.Button(text="Reset", command=reset_timer)
 reset_button.grid(row=2, column=2)
 
 checks_label = tkinter.Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25, "bold"))
