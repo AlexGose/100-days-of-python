28aa472dcb7f24a18cac767f28cee41184f92e24
Alex Gose <alexgose@users.noreply.github.com>
2023-04-12 10:57:10 -0400

    Add top label, buttons, and check mark

--- day28-pomodoro/main.py
+++ day28-pomodoro/main.py
@@ -24,6 +24,18 @@ canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+timer_label = tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25, "bold"))
+timer_label.grid(row=0, column=1)
+
+start_button = tkinter.Button(text="Start")
+start_button.grid(row=2, column=0)
+
+reset_button = tkinter.Button(text="Reset")
+reset_button.grid(row=2, column=2)
+
+checks_label = tkinter.Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25, "bold"))
+checks_label.grid(row=2, column=1)
 
 window.mainloop()
