114b241b354323811350da80fdd87bed1609105d
Alex Gose <alexgose@users.noreply.github.com>
2023-04-18 11:00:43 -0400

    Add wrong and right buttons

--- day31-flashcards/main.py
+++ day31-flashcards/main.py
@@ -7,12 +7,22 @@ if __name__ == "__main__":
     window.title("Flashy")
     window.configure(padx=50, pady=50, background=BACKGROUND_COLOR)
 
-    canvas = tkinter.Canvas(height=526, width=800, bg=BACKGROUND_COLOR,
+    canvas = tkinter.Canvas(height=526, width=800, background=BACKGROUND_COLOR,
                             highlightthickness=0)
     canvas.configure()
 
     logo_img = tkinter.PhotoImage(file="images/card_front.png")
     canvas.create_image(400, 263, image=logo_img)
-    canvas.grid(row=0, column=0)
+    canvas.grid(row=0, column=0, columnspan=2)
+
+    wrong_img = tkinter.PhotoImage(file="images/wrong.png")
+    wrong_button = tkinter.Button(image=wrong_img, background=BACKGROUND_COLOR,
+                                  highlightthickness=0)
+    wrong_button.grid(row=1, column=0)
+
+    right_img = tkinter.PhotoImage(file="images/right.png")
+    right_button = tkinter.Button(image=right_img, background=BACKGROUND_COLOR,
+                                  highlightthickness=0)
+    right_button.grid(row=1, column=1)
 
     window.mainloop()
