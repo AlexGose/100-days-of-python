ad3509c8da0a78151e5729ecaa425ab3b6a7b848
Alex Gose <alexgose@users.noreply.github.com>
2023-04-18 10:55:50 -0400

    Add front card image to window

--- day31-flashcards/main.py
+++ day31-flashcards/main.py
@@ -1,2 +1,18 @@
+import tkinter
 BACKGROUND_COLOR = "#B1DDC6"
 
+
+if __name__ == "__main__":
+    window = tkinter.Tk()
+    window.title("Flashy")
+    window.configure(padx=50, pady=50, background=BACKGROUND_COLOR)
+
+    canvas = tkinter.Canvas(height=526, width=800, bg=BACKGROUND_COLOR,
+                            highlightthickness=0)
+    canvas.configure()
+
+    logo_img = tkinter.PhotoImage(file="images/card_front.png")
+    canvas.create_image(400, 263, image=logo_img)
+    canvas.grid(row=0, column=0)
+
+    window.mainloop()
