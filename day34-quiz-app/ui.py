ab6cd5a56252dc5d255e8509bde2887bd58c01ef
Alex Gose <alexgose@users.noreply.github.com>
2023-04-22 16:06:36 -0400

    Add quiz questions to the canvas

--- day34-quiz-app/ui.py
+++ day34-quiz-app/ui.py
@@ -1,11 +1,14 @@
 import tkinter
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = tkinter.Tk()
         self.window.title("Quizzler")
         self.window.configure(background=THEME_COLOR, padx=20, pady=20)
@@ -17,7 +20,12 @@ class QuizInterface:
 
         self.canvas = tkinter.Canvas(width=300, height=250, background="white")
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
-        self.canvas.create_text(150, 125, text="Text goes here", fill=THEME_COLOR, font=('Ariel', 20, 'italic'))
+        self.question_text = self.canvas.create_text(150,
+                                                     125,
+                                                     width=280,
+                                                     text="Text goes here",
+                                                     fill=THEME_COLOR,
+                                                     font=('Ariel', 20, 'italic'))
 
         checkmark_img = tkinter.PhotoImage(file="images/true.png")
         self.true_button = tkinter.Button(image=checkmark_img, highlightthickness=0)
@@ -27,4 +35,9 @@ class QuizInterface:
         self.false_button = tkinter.Button(image=x_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.itemconfig(self.question_text, text=self.quiz.next_question())
