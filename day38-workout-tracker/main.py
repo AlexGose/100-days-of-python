0b33323c8572b3e488d64758beaaf550e21d23d3
Alex Gose <alexgose@users.noreply.github.com>
2023-04-29 15:21:53 -0400

    Add a row to spreadsheet using Sheety

--- day38-workout-tracker/main.py
+++ day38-workout-tracker/main.py
@@ -1,7 +1,9 @@
 import requests
 import os
+import datetime
 
-
+SHEETY_ENDPOINT = os.getenv('SHEETY_ENDPOINT')
+SHEETY_AUTH_TOKEN = os.getenv('SHEETY_AUTH_TOKEN')
 NUTRITIONIX_APP_ID = os.getenv('NUTRITIONIX_APP_ID')
 NUTRITIONIX_APP_KEY = os.getenv('NUTRITIONIX_APP_KEY')
 NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
@@ -24,4 +26,27 @@ parameters = {
     'age': age
 }
 response = requests.post(url=NUTRITIONIX_ENDPOINT, json=parameters, headers=headers)
-print(response.text)
+response.raise_for_status()
+exercise_data = response.json()
+
+date = datetime.datetime.now().strftime('%m/%d/%Y')
+time = datetime.datetime.now().strftime('%H:%M:%S')
+
+for exercise in exercise_data['exercises']:
+    duration_min = exercise['duration_min']
+    calories = exercise['nf_calories']
+    name = exercise['name'].title()
+    parameters = {
+        "workout": {
+            'date': date,
+            'time': time,
+            'exercise': name,
+            'duration': duration_min,
+            'calories': calories
+        }
+    }
+    headers = {
+        'Authorization': f"Bearer {SHEETY_AUTH_TOKEN}"
+    }
+    response = requests.post(url=SHEETY_ENDPOINT, json=parameters, headers=headers)
+    response.raise_for_status()
