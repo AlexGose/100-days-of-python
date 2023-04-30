4b5f2c611db4d0a73e9658b6f2c709962078d487
Alex Gose <alexgose@users.noreply.github.com>
2023-04-30 14:17:40 -0400

    Lookup IATA code for each city in the spreadsheet

--- day39-flight-deal-finder/data_manager.py
+++ day39-flight-deal-finder/data_manager.py
@@ -1,3 +1,16 @@
+import requests
+import os
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.auth_token = os.getenv('SHEETY_AUTH_TOKEN')
+        self.endpoint = os.getenv('SHEETY_ENDPOINT')
+
+    def get_rows(self):
+        headers = {
+            'Authorization': f"Bearer {self.auth_token}"
+        }
+        response = requests.get(url=self.endpoint, headers=headers)
+        response.raise_for_status()
+        return response.json()['prices']
--- day39-flight-deal-finder/main.py
+++ day39-flight-deal-finder/main.py
@@ -1,6 +1,10 @@
 from flight_search import FlightSearch
+from data_manager import DataManager
 
 
 if __name__ == '__main__':
     fs = FlightSearch()
-    fs.get_iata('Paris')
+    dm = DataManager()
+    for row in dm.get_rows():
+        code = fs.get_iata(row['city'])
+        print(row['id'], row['city'], code)
