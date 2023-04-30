544f46a2c0f68344a49fa1e46b2317051dbfda8a
Alex Gose <alexgose@users.noreply.github.com>
2023-04-30 14:34:30 -0400

    Add IATA codes to spreadsheet

--- day39-flight-deal-finder/main.py
+++ day39-flight-deal-finder/main.py
@@ -6,5 +6,4 @@ if __name__ == '__main__':
     fs = FlightSearch()
     dm = DataManager()
     for row in dm.get_rows():
-        code = fs.get_iata(row['city'])
-        print(row['id'], row['city'], code)
+        dm.edit_iata_code(row['id'], fs.get_iata(row['city']))
