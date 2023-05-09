de021aa96b326e5ff54215bc028490ac74e20bc1
Alex Gose <alexgose@users.noreply.github.com>
2023-05-09 16:55:31 -0400

    Refactor code into functions

--- day46-spotify-playlist/main.py
+++ day46-spotify-playlist/main.py
@@ -2,10 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 
 
-if __name__ == '__main__':
+def get_billboard_url():
     year_str = input('What year do you want to time travel to?  Type the date in this format YYYY-MM-DD: ')
-    url = f"https://www.billboard.com/charts/hot-100/{year_str}/"
+    return f"https://www.billboard.com/charts/hot-100/{year_str}/"
 
+
+def get_song_list(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -15,5 +17,11 @@ if __name__ == '__main__':
         row = soup.select(selector=f'ul[data-detail-target="{index}"]')
         songs.append(row[0].find("h3").getText().strip())
         artists.append(row[0].select("li ul li span")[0].getText().strip())
-    print(artists)
-    print(songs)
+    return songs, artists
+
+
+if __name__ == '__main__':
+    song_list, artist_list = get_song_list(get_billboard_url())
+
+    print(artist_list)
+    print(song_list)
