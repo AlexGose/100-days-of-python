56c32b349b6e16d9afdf15cfcecb0e92b88ff87d
Alex Gose <alexgose@users.noreply.github.com>
2023-05-17 12:54:23 -0400

    Implement login method

--- day52-instagram-bot/insta_follower.py
+++ day52-instagram-bot/insta_follower.py
@@ -1,11 +1,14 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import os
+import time
 
 CHROMEDRIVER_PATH = "/opt/chromedriver"
 INSTAGRAM_USERNAME = os.getenv('INSTAGRAM_USERNAME')
 INSTAGRAM_PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
 SIMILAR_INSTAGRAM_ACCOUNT = os.getenv('SIMILAR_INSTAGRAM_ACCOUNT')
+
 
 class InstaFollower:
     def __init__(self):
@@ -14,7 +17,15 @@ class InstaFollower:
         self.password = INSTAGRAM_PASSWORD
 
     def login(self):
-        pass
+        self.driver.get("https://www.instagram.com/accounts/login/")
+        time.sleep(10)
+        user_name = self.driver.find_element(By.NAME, "username")
+        user_name.send_keys(self.username)
+        password = self.driver.find_element(By.NAME, "password")
+        password.send_keys(self.password)
+        login_button = self.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
+        login_button.click()
+        time.sleep(10)
 
     def find_followers(self):
         pass
