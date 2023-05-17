15e3f7a022afae421dcc99af8b1e1f0f8ce3e402
Alex Gose <alexgose@users.noreply.github.com>
2023-05-17 11:35:10 -0400

    Create bot class and method stubs

---
+++ day52-instagram-bot/InstaFollower.py
@@ -0,0 +1,30 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+import os
+
+CHROMEDRIVER_PATH = "/opt/chromedriver"
+INSTAGRAM_USERNAME = os.getenv('INSTAGRAM_USERNAME')
+INSTAGRAM_PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
+
+
+class InstaFollower:
+    def __init__(self):
+        self.driver = Chrome(service=Service(CHROMEDRIVER_PATH))
+        self.username = INSTAGRAM_USERNAME
+        self.password = INSTAGRAM_PASSWORD
+
+    def login(self):
+        pass
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
+
+
+if __name__ == '__main__':
+    follower_bot = InstaFollower()
+    follower_bot.login()
+    follower_bot.find_followers()
+    follower_bot.follow()
