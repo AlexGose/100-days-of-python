132ce1f37321094248c8df3cec0391a5ba84dd76
Alex Gose <alexgose@users.noreply.github.com>
2023-05-24 11:10:40 -0400

    Use Jinja loops to add blog titles

--- day57-flask-jinja-blog/server.py
+++ day57-flask-jinja-blog/server.py
@@ -30,5 +30,12 @@ def guess(name):
                            age=guess_age(name))
 
 
+@app.route('/blog')
+def blog():
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    all_posts = response.json()
+    return render_template('blog.html', posts=all_posts)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
