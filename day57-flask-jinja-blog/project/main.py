b51376729f66dc1c8f2041c74092f62fc421f854
Alex Gose <alexgose@users.noreply.github.com>
2023-05-24 13:08:14 -0400

    Render post content

--- day57-flask-jinja-blog/project/main.py
+++ day57-flask-jinja-blog/project/main.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template
 import requests
 
+response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+all_posts = response.json()
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-    all_posts = response.json()
     return render_template("index.html", posts=all_posts)
 
 
 @app.route('/post/<num>')
 def get_post(num):
-    return render_template("post.html", num=num)
+    return render_template("post.html", post=all_posts[int(num)-1], num=num)
 
 
 if __name__ == "__main__":
