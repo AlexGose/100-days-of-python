08fd8039c172c7c4373dda1065dad7fa3381d1e7
Alex Gose <alexgose@users.noreply.github.com>
2023-05-24 11:46:41 -0400

    Render titles and subtitles on the home page

--- day57-flask-jinja-blog/project/main.py
+++ day57-flask-jinja-blog/project/main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts)
+
+
+@app.route('/blog/<num>')
+def get_blog(num):
+    return render_template("post.html", num=num)
 
 
 if __name__ == "__main__":
