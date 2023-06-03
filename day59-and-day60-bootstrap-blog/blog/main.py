9b05ac4b7a12ffefddbe09999d6adafb80a41dff
Alex Gose <alexgose@users.noreply.github.com>
2023-06-03 11:33:49 -0400

    Send email with contact page submission

--- day59-and-day60-bootstrap-blog/blog/main.py
+++ day59-and-day60-bootstrap-blog/blog/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
 
 response = requests.get('https://api.npoint.io/2a4be25f4f1d7f080ec5')
 blogs_data = response.json()
@@ -22,11 +24,25 @@ def contact():
     if request.method == 'GET':
         return render_template('contact.html', method='GET')
     else:
-        print(request.form['name'])
-        print(request.form['email'])
-        print(request.form['phone'])
-        print(request.form['message'])
+        send_email(request.form['name'], request.form['email'],
+                   request.form['phone'], request.form['message'])
         return render_template('contact.html', method='POST')
+
+
+def send_email(name, email, phone, message):
+    message_string = f"Subject: Message from {name}\n\nname:{name}\n"
+    message_string += f"email:{email}\nphone:{phone}\nmessage:{message}"
+
+    my_email = os.getenv("EMAIL_ADDRESS")
+    password = os.getenv("EMAIL_PASSWORD")
+    smtp_server = os.getenv("SMTP_SERVER")
+    smtp_port = int(os.getenv("SMTP_PORT"))
+
+    with smtplib.SMTP(smtp_server, port=smtp_port) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=my_email,
+                            msg=message_string)
 
 
 @app.route('/post/<int:post_id>')
