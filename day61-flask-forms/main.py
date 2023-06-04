be7c06ddc777d4dba1ea9409a383412b7ab6b229
Alex Gose <alexgose@users.noreply.github.com>
2023-06-04 13:45:15 -0400

    Make email and password required for login

--- day61-flask-forms/main.py
+++ day61-flask-forms/main.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.secret_key = "some secret string"
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = PasswordField('Password')
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
 
 
