93d54da08cba03bd44d587c14bee2d6c8a9d4a81
Alex Gose <alexgose@users.noreply.github.com>
2023-06-09 16:38:21 -0400

    Use Flask-SQLAlchemy to perform CRUD operations
    This avoids using the legacy functions `Model.query`
    and `session.query`.  See "Modifying and Querying Data":
    https://flask-sqlalchemy.palletsprojects.com/en/3.0.x/

--- day63-book-review-site/SQLite/main.py
+++ day63-book-review-site/SQLite/main.py
@@ -15,17 +15,49 @@ class Book(db.Model):
     author = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.Float(), nullable=False)
 
+    def __repr__(self):
+        return f"<Book id:{self.id}, title:{self.title}, author:{self.author}, rating:{self.rating}>"
 
-db.create_all()
 
-book = Book()
-book.id = 1
-book.title = 'Harry Potter'
-book.author = 'J. K. Rowling'
-book.rating = 9.3
+db.create_all()  # create database file and table
+
+# create a new record
+book = Book(id=1, title='Harry Potter', author='J. K. Rowling', rating=9.3)
+
+# Alternative:
+# book = Book()
+# book.id = 1
+# book.title = 'Harry Potter'
+# book.author = 'J. K. Rowling'
+# book.rating = 9.3
 
 db.session.add(book)
 db.session.commit()
+
+# read all records (bd.session.query is legacy)
+all_books = db.session.execute(db.select(Book).order_by(Book.author)).scalars().all()
+print(all_books)
+
+# read a particular record
+particular_book = db.first_or_404(db.select(Book).filter_by(title='Harry Potter'))
+print(particular_book)
+
+# update a particular record
+particular_book.title = 'Harry Potter and the Chamber of Secrets'
+db.session.commit()
+print(particular_book)  # checked the database too
+
+# update a record by primary key
+book_id = 1
+book_to_update = db.one_or_404(db.select(Book).filter_by(id=1))
+book_to_update.title = 'Harry Potter and the Goblet of Fire'
+print(book_to_update)
+
+# delete a particular record by primary key
+book_id = 1
+book_to_delete = db.one_or_404(db.select(Book).filter_by(id=1))
+db.session.delete(book_to_delete)
+db.session.commit()  # checked the database too
 
 # import sqlite3
 #
