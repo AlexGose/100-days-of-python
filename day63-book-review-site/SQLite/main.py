5fbf09529f2d61e1b01381bc6e9a5b414d346bd5
Alex Gose <alexgose@users.noreply.github.com>
2023-06-09 15:21:20 -0400

    Create database and books table and add record
    Use SQLAlchemy this time

--- day63-book-review-site/SQLite/main.py
+++ day63-book-review-site/SQLite/main.py
@@ -1,22 +1,51 @@
-import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-db = sqlite3.connect('books-collection.db')
 
-cursor = db.cursor()
-create_table_sql = """
-CREATE TABLE books (
-    id INTEGER PRIMARY KEY,
-    title varchar(250) NOT NULL UNIQUE,
-    author varchar(250) NOT NULL,
-    rating FLOAT NOT NULL
-)
-"""
-cursor.execute(create_table_sql)
+app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+db = SQLAlchemy()
+db.init_app(app)
 
-add_book_sql = """
-INSERT INTO books
-  VALUES (1, 'Tale of Two Cities', 'Charles Dickens', 9.3)
-"""
 
-cursor.execute(add_book_sql)
-db.commit()
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float(), nullable=False)
+
+
+db.create_all()
+
+book = Book()
+book.id = 1
+book.title = 'Harry Potter'
+book.author = 'J. K. Rowling'
+book.rating = 9.3
+
+db.session.add(book)
+db.session.commit()
+
+# import sqlite3
+#
+# db = sqlite3.connect('books-collection.db')
+#
+# cursor = db.cursor()
+# create_table_sql = """
+# CREATE TABLE books (
+#     id INTEGER PRIMARY KEY,
+#     title varchar(250) NOT NULL UNIQUE,
+#     author varchar(250) NOT NULL,
+#     rating FLOAT NOT NULL
+# )
+# """
+# cursor.execute(create_table_sql)
+#
+# add_book_sql = """
+# INSERT INTO books
+#   VALUES (1, 'Tale of Two Cities', 'Charles Dickens', 9.3)
+# """
+#
+# cursor.execute(add_book_sql)
+# db.commit()
