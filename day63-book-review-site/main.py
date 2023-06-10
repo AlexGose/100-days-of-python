b2f52f3808e535934a65732b3ab95b6bb3e6adcd
Alex Gose <alexgose@users.noreply.github.com>
2023-06-10 10:30:25 -0400

    Use SQLite database to store book info

--- day63-book-review-site/main.py
+++ day63-book-review-site/main.py
@@ -1,29 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.db'
+db = SQLAlchemy()
+db.init_app(app)
 
-all_books = []
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float(), nullable=False)
+
+    def __repr__(self):
+        return f"<Book id:{self.id}, name:{self.name}, author:{self.author}, rating:{self.rating}>"
+
+
+db.create_all()  # create database file and table
 
 
 @app.route('/')
 def home():
+    all_books = db.session.execute(db.select(Book).order_by(Book.name)).scalars().all()
     return render_template('index.html', all_books=all_books)
 
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
-    global all_books
     if request.method == 'POST':
-        new_book = {
-            'name': request.form['name'],
-            'author': request.form['author'],
-            'rating': request.form['rating']
-        }
-        all_books += [new_book]
+        new_book = Book(name=request.form['name'], author=request.form['author'], rating=request.form['rating'])
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template('add.html')
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
