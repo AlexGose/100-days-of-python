74011ba129ad594611c997e4040bda8c265d5f2b
Alex Gose <alexgose@users.noreply.github.com>
2023-06-12 13:45:09 -0400

    Make movie data visible on the home page

--- day64-top-movies-site/main.py
+++ day64-top-movies-site/main.py
@@ -29,10 +29,24 @@ class Movie(db.Model):
 
 db.create_all()
 
+new_movie = Movie(
+    title="Phone Booth",
+    year=2002,
+    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    rating=7.3,
+    ranking=10,
+    review="My favourite character was the caller.",
+    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+)
+
+db.session.add(new_movie)
+db.session.commit()
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movie = db.one_or_404(db.select(Movie).filter_by(id=1))
+    return render_template("index.html", movie=movie)
 
 
 if __name__ == '__main__':
